cbc2abf3796f9701a38ee18d8dfc73306d3d8f5e
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-12-20 15:50:05 -0800

    Set up santiy headless CMS, Add schema for pizza

---
+++ schemas/pizza.js
@@ -0,0 +1,44 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  // Computer Name
+  name: 'pizza',
+  // visible title
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      // minimum $10 pizza, maximum $50
+      validation: (Rule) => Rule.min(1000).max(50000),
+      // TODO: Add custom input component
+    },
+  ],
+};
